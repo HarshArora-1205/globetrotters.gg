e65ee763e47e379de7f10e41164a359cf68b1082
Harsh Arora <iamharsharora1205@gmail.com>
2024-12-18 22:57:54 +0530

    init: font + plugins setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,68 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Dela_Gothic_One } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+// !--------------- Google Font ------------->
+const dela = Dela_Gothic_One({
+  weight: ["400"],
+  variable: "--font-dela-gothic",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+// !--------------- Local Font ------------->
+// !--------------- cc: Dafont ------------->
+const gilroy = localFont({
+  src: [
+    {
+      path: "/fonts/Gilroy-Thin.ttf",
+      weight: "100",
+    },
+    {
+      path: "/fonts/Gilroy-UltraLight.ttf",
+      weight: "200",
+    },
+    {
+      path: "/fonts/Gilroy-Light.ttf",
+      weight: "300",
+    },
+    {
+      path: "/fonts/Gilroy-Regular.ttf",
+      weight: "400",
+    },
+    {
+      path: "/fonts/Gilroy-Medium.ttf",
+      weight: "500",
+    },
+    {
+      path: "/fonts/Gilroy-SemiBold.ttf",
+      weight: "600",
+    },
+    {
+      path: "/fonts/Gilroy-Bold.ttf",
+      weight: "700",
+    },
+    {
+      path: "/fonts/Gilroy-ExtraBold.ttf",
+      weight: "800",
+    },
+    {
+      path: "/fonts/Gilroy-Black.ttf",
+      weight: "900",
+    },
+    {
+      path: "/fonts/Gilroy-Heavy.ttf",
+      weight: "950",
+    },
+  ],
+  variable: "--font-gilroy",
 });
 
 export const metadata: Metadata = {
   title: "Globetrotters",
-  description: "Effortlessly plan your next trip with AI. Explore destinations, craft personalized itineraries, and enjoy seamless travel planning from start to finish.",
+  description:
+    "Effortlessly plan your next trip with AI. Explore destinations, craft personalized itineraries, and enjoy seamless travel planning from start to finish.",
 };
 
 export default function RootLayout({
@@ -25,9 +72,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${gilroy.variable} ${dela.variable} antialiased`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
